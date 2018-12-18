eca53990e1f9804ef95627c78402af902d5f50a5
Raymond Rupiah <rupiray@yahoo.com>
2018-12-18 08:22:49 +0000

    Added more views

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from catalogue.models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 def index(request):
 
@@ -22,3 +24,21 @@ def index(request):
 
     # Render HTML template
     return render(request, 'index.html', context=context)
+
+# Book list
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+# Book detail
+class BookDetailView(generic.DetailView):
+    model = Book
+
+# Author list
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+# Author detail
+class AuthorDetailView(generic.DetailView):
+    model = Author
