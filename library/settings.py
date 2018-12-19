914ce7f36f4646035462982a1b291eba12b0985c
Raymond Rupiah <rupiray@yahoo.com>
2018-12-19 14:54:59 +0000

    New app name

--- library/settings.py
+++ library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'lgekcozf-=6hx!x-w^5dzhv462)81z
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['my-library']
+ALLOWED_HOSTS = ['my-bookie-books-library']
 
 
 # Application definition
